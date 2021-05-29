571c663fca5cf9424f712ab1bda5e27b18042914
root <root@pve.home.local>
2021-05-29 13:59:34 -0400

    remove cloudprint and add sftp sync

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# Usage: bash build.sh [dirname]
+# Example: bash build.sh cups
 TAG="$1"
 BASEDIR=$(dirname "$0")
 DIR=$BASEDIR/$TAG
 docker build --pull --tag $TAG $DIR
-
--- push.sh
+++ push.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Usage: bash push.sh [dirname]
+# Example: bash push.sh cups
 TAG="$1"
 docker tag $TAG asiellb/$TAG
 docker push asiellb/$TAG
